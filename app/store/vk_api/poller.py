1c63d2930ba5046d4442a4c3a0d652d2c7307dc6
Никита <nik.ysmanof@mail.ru>
2023-03-07 23:59:50 +0500

    test rabbitMQ

--- app/store/vk_api/poller.py
+++ app/store/vk_api/poller.py
@@ -1,7 +1,9 @@
 import asyncio
 from asyncio import Task
 from logging import getLogger
-from typing import Optional
+
+from aio_pika import connect_robust
+from aio_pika.patterns import Master
 
 from app.store import Store
 
@@ -12,10 +14,12 @@ class Poller:
         self.logger = getLogger("poller")
         self.is_running = False
         self.poll_task: Task | None = None
+        self.worker: Task | None = None
         self.handle_tasks: list[Task] | list = []
 
     async def start(self):
         self.is_running = True
+        self.worker = asyncio.create_task(self.working())
         self.poll_task = asyncio.create_task(self.poll())
 
     async def stop(self):
@@ -26,11 +30,43 @@ class Poller:
             if isinstance(task, Exception):
                 self.logger.error(str(task))
 
+        self.worker.cancel()
+        await self.worker
         await self.poll_task
 
     async def poll(self):
-        while self.is_running:
-            updates = await self.store.tg_api.poll()
-            task = asyncio.create_task(self.store.bots_manager.handle_updates(updates))
-            task.add_done_callback(lambda x: self.logger.info(f"Выполнено {x}"))
-            self.handle_tasks.append(task)
+        self.connection_master = await connect_robust(
+            "amqp://guest:guest@127.0.0.1/?name=aio-pika%20master",
+        )
+        async with self.connection_master:
+            channel = await self.connection_master.channel()
+            master = Master(channel)
+
+            while self.is_running:
+                updates = await self.store.tg_api.poll()
+
+                if updates:
+                    await master.create_task(
+                        "updates_poll", kwargs=dict(updates=updates)
+                    )
+
+                # task = asyncio.create_task(self.store.bots_manager.handle_updates(updates))
+                # task.add_done_callback(lambda x: self.logger.info(f"Выполнено {x}"))
+                # self.handle_tasks.append(task)
+
+    async def working(self):
+        self.connection_worker = await connect_robust(
+            "amqp://guest:guest@127.0.0.1/?name=aio-pika%20worker",
+        )
+
+        channel = await self.connection_worker.channel()
+        master = Master(channel)
+        await master.create_worker("updates_poll", self.workering, auto_delete=True)
+
+        try:
+            await asyncio.Future()
+        finally:
+            await self.connection_worker.close()
+
+    async def workering(self, *, updates: list) -> None:
+        await self.store.bots_manager.handle_updates(updates)
